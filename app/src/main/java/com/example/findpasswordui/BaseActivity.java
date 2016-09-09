597beefeb24a0a2db477e948c47a4dd2f2fcf0b7
Tony <tony@gmail.com>
2016-09-09 09:40:31 +0800

    new commit add two activities

---
+++ app/src/main/java/com/example/findpasswordui/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.example.findpasswordui;
+
+import android.app.Activity;
+
+/**
+ * Created by Administrator on 2016/9/9.
+ */
+public class BaseActivity extends Activity {
+
+}
---
+++ app/src/main/java/com/example/findpasswordui/LoginActivity.java
@@ -0,0 +1,8 @@
+package com.example.findpasswordui;
+
+/**
+ * Created by wangyinmeng on 2016/9/9.
+ */
+public class LoginActivity extends BaseActivity {
+
+}
